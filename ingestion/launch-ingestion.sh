ea9495e2986954929875ac67e6bb66a246bdf7da
Clément <clement@numericite.eu>
2025-04-08 16:53:17 +0200

    fix: full test data noisemap 033

--- ingestion/launch-ingestion.sh
+++ ingestion/launch-ingestion.sh
@@ -8,6 +8,14 @@ python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_F_C_LN_S_033/N_BRUIT
 echo '--------------------------------------------------------------------------'
 python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_R_C_LD_S_033/N_BRUIT_ZBRD_INFRA_R_C_LD_S_033.shp raw_noisemap
 echo '--------------------------------------------------------------------------'
+python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_R_A_LN_S_033/N_BRUIT_ZBRD_INFRA_R_A_LN_S_033.shp raw_noisemap
+echo '--------------------------------------------------------------------------'
+python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_F_A_LD_S_033/N_BRUIT_ZBRD_INFRA_F_A_LD_S_033.shp raw_noisemap
+echo '--------------------------------------------------------------------------'
+python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_F_A_LN_S_033/N_BRUIT_ZBRD_INFRA_F_A_LN_S_033.shp raw_noisemap
+echo '--------------------------------------------------------------------------'
+python ingest_shapefiles.py inputs/noise/N_BRUIT_ZBRD_INFRA_R_A_LD_S_033/N_BRUIT_ZBRD_INFRA_R_A_LD_S_033.shp raw_noisemap
+echo '--------------------------------------------------------------------------'
 python ingest_shapefiles.py inputs/soundclassification/SNCF/Class_sonore_DDTM33_SNCF.shp raw_soundclassification_sncf --if-exists replace
 echo '--------------------------------------------------------------------------'
 python ingest_shapefiles.py inputs/soundclassification/ROUTIER/Class_sonore_DDTM33_routier.shp raw_soundclassification_routier --if-exists replace
