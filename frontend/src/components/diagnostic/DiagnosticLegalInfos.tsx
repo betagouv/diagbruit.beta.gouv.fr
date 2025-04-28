e118c78447668fe160f0aa371a92fcc675992462
Clément <clement@numericite.eu>
2025-04-28 16:45:59 +0200

    feat(frontend): update diagnostic legal infos zone

--- frontend/src/components/diagnostic/DiagnosticLegalInfos.tsx
+++ frontend/src/components/diagnostic/DiagnosticLegalInfos.tsx
@@ -3,6 +3,7 @@ import { DiagnosticItem } from "../../utils/types";
 import { fr } from "@codegouvfr/react-dsfr";
 import { getReadableSource } from "../../utils/tools";
 import { Accordion } from "@codegouvfr/react-dsfr/Accordion";
+import { LEGAL_TEXTS } from "../../utils/texts/legal";
 
 type DiagnosticLegalInfosProps = {
   diagnosticItem: DiagnosticItem;
@@ -14,23 +15,14 @@ const DiagnosticLegalInfos = ({
   const { cx, classes } = useStyles();
 
   const {
-    diagnostic: { soundclassification_intersections },
+    diagnostic: { soundclassification_intersections, air_intersections },
   } = diagnosticItem;
 
   const getSoundClassificationHeaderLine = () => {
-    if (!soundclassification_intersections.length)
-      return "La parcelle n'est pas impactée par un secteur du classement sonore.";
-
-    let soundClassificationText = "";
-
     const hasMany = soundclassification_intersections.length > 1;
-    soundClassificationText += `La parcelle est impactée par ${
-      hasMany ? "plusieurs" : "un"
-    } secteur${hasMany ? "s" : ""} du classement sonore.`;
-
-    soundclassification_intersections.forEach((soundClassification) => {});
-
-    return soundClassificationText;
+    return hasMany
+      ? LEGAL_TEXTS.SOUNDCLASSIFICATION.INTRODUCTION.MULTIPLE_IMPACTS
+      : LEGAL_TEXTS.SOUNDCLASSIFICATION.INTRODUCTION.SINGLE_IMPACT;
   };
 
   return (
@@ -40,36 +32,69 @@ const DiagnosticLegalInfos = ({
           <img src="/images/document.svg" width={80} height={80} />
         </div>
         <div className={classes.content}>
-          <h2 className={cx("fr-h5")}>1. Informations réglementaires</h2>
-          <p className={cx(classes.section, fr.cx("fr-mb-0"))}>
-            {getSoundClassificationHeaderLine()}
-            {soundclassification_intersections.length > 0 && (
-              <>
-                <ul>
-                  {soundclassification_intersections.map(
-                    (soundClassification, index) => {
-                      const { codeinfra, typesource, distance } =
-                        soundClassification;
-                      return (
-                        <li key={index}>
-                          La parcelle se situe à <b>{distance}</b> mètre
-                          {distance > 1 ? "s" : ""} de la source{" "}
-                          <b>"{codeinfra}"</b> de catégorie{" "}
-                          <b>"{getReadableSource(typesource)}"</b>.
-                        </li>
-                      );
-                    }
-                  )}
+          <h2 className={fr.cx("fr-h5")}>1. Informations réglementaires</h2>
+          {soundclassification_intersections.length > 0 && (
+            <>
+              <h3 className={fr.cx("fr-text--lg", "fr-mb-4v", "fr-mt-4v")}>
+                Classement sonore
+              </h3>
+              <p className={cx(classes.section, fr.cx("fr-mb-0"))}>
+                {getSoundClassificationHeaderLine()}
+                {soundclassification_intersections.length > 0 && (
+                  <>
+                    <ul>
+                      {soundclassification_intersections.map(
+                        (soundClassification, index) => {
+                          const { codeinfra, typesource, distance } =
+                            soundClassification;
+                          return (
+                            <li key={index}>
+                              La parcelle se situe à <b>{distance}</b> mètre
+                              {distance > 1 ? "s" : ""} de la source{" "}
+                              <b>"{codeinfra}"</b> de catégorie{" "}
+                              <b>"{getReadableSource(typesource)}"</b>.
+                            </li>
+                          );
+                        }
+                      )}
+                    </ul>
+                    <p className={fr.cx("fr-mb-0")}>
+                      {LEGAL_TEXTS.SOUNDCLASSIFICATION.DETAILS.NOTICE}
+                    </p>
+                  </>
+                )}
+              </p>
+            </>
+          )}
+          {air_intersections.length > 0 && (
+            <>
+              <h3 className={fr.cx("fr-text--lg", "fr-mb-4v", "fr-mt-8v")}>
+                Plan d'exposition au bruit aérien
+              </h3>
+              <p className={cx(classes.section, fr.cx("fr-mb-0"))}>
+                La parcelle est situé dans un secteur du Plan d’Exposition au
+                Bruit de "<b>{air_intersections[0].nom}</b>" :
+                <ul className={fr.cx("fr-mb-0")}>
+                  <li>zone : {air_intersections[0].zone}</li>
+                  <li>
+                    Code de l’urbanisme dédié au PEB :{" "}
+                    <a
+                      href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000031210273"
+                      target="_blank"
+                    >
+                      article L112-10 du code de l’urbanisme
+                    </a>
+                  </li>
                 </ul>
-                <p className={fr.cx("fr-mb-0")}>
-                  Selon la distance de la construction à la source, la présence
-                  de bâtiments ou de protections phoniques à la source,
-                  l’isolation acoustique règlementaire peut varier (cf lien
-                  ci-après).
-                </p>
-              </>
+              </p>
+            </>
+          )}
+          {air_intersections.length === 0 &&
+            soundclassification_intersections.length === 0 && (
+              <p className={cx(classes.section, fr.cx("fr-mb-0"))}>
+                {LEGAL_TEXTS.NO_IMPACT}
+              </p>
             )}
-          </p>
         </div>
       </div>
       <Accordion
---
+++ frontend/src/utils/texts/legal.ts
@@ -0,0 +1,18 @@
+// src/constants/legal.ts
+
+export const LEGAL_TEXTS = {
+  NO_IMPACT:
+    "La parcelle n’est concernée par aucune obligation réglementaire acoustique liée au bruit des transports (classement sonore ; Plan d’Exposition au Bruit).",
+  SOUNDCLASSIFICATION: {
+    INTRODUCTION: {
+      SINGLE_IMPACT:
+        "La parcelle est impactée par un secteur du classement sonore.",
+      MULTIPLE_IMPACTS:
+        "La parcelle est impactée par plusieurs secteurs du classement sonore.",
+    },
+
+    DETAILS: {
+      NOTICE: `Selon la distance de la construction à la source, la présence de bâtiments ou de protections phoniques à la source, l’isolation acoustique règlementaire peut varier (cf lien ci-après).`,
+    },
+  },
+};
