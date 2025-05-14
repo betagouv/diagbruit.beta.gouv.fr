5f9afe408f82b6d71bcd868531a9bf24e5cc11da
Clément <clement@numericite.eu>
2025-05-14 15:50:26 +0200

    feat(fastapi): retrieve recommendations from cms

--- fastapi/app/algorithm/modules/recommendations.py
+++ fastapi/app/algorithm/modules/recommendations.py
@@ -1,39 +1,47 @@
-import yaml
-from pathlib import Path
+import os
+import requests
 from typing import List
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_BASE_URL = os.getenv("STRAPI_URL", "http://localhost:1337")
+API_RECOMMENDATIONS_ENDPOINT = f"{API_BASE_URL}/api/recommendations?populate=*"
 
 
-def load_recommendations() -> List[dict]:
+def fetch_recommendations_from_api() -> List[dict]:
     """
-    Load the YAML file containing the recommendations.
+    Fetch recommendations from the API.
     """
-    base_path = Path(__file__).resolve().parent.parent.parent / "references"
-    filepath = base_path / "recommendations.yaml"
-
-    with open(filepath, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f)
+    response = requests.get(API_RECOMMENDATIONS_ENDPOINT)
+    response.raise_for_status()
+    data = response.json()
+    return data.get("data", [])
 
 
 def get_recommendations_by_score(score: float) -> List[dict]:
     """
     Return a list of recommendations that match the given score.
     """
-    recommendations = load_recommendations()
+    recommendations = fetch_recommendations_from_api()
 
     def is_valid(rec):
-        s = rec.get("score", {})
-        if "value_gte" in s and score < s["value_gte"]:
+        cond = rec.get("conditions", {})
+        score_gte = cond.get("score_gte")
+        score_lte = cond.get("score_lte")
+
+        if score_gte is not None and score < score_gte:
             return False
-        if "value_lte" in s and score > s["value_lte"]:
+        if score_lte is not None and score > score_lte:
             return False
         return True
 
     return [
-            {
-                "title": r["title"],
-                "category": r["category"],
-                "content": r["content"],
-                "links": r["links"]
-            }
-            for r in recommendations if is_valid(r)
-        ]
+        {
+            "title": r["title"],
+            "category": r.get("category").get('title'),
+            "content": r["content"],
+            "links": r.get("links", []),
+        }
+        for r in recommendations if is_valid(r)
+    ]
