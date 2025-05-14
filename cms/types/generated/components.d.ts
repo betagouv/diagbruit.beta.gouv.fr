284c6857c9d05645ce83121f8dd464b34cf3bb75
Clément <clement@numericite.eu>
2025-05-14 12:14:10 +0200

    feat(cms): design first model for recommendations

--- cms/types/generated/components.d.ts
+++ cms/types/generated/components.d.ts
@@ -1,3 +1,25 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface GlobalConditions extends Struct.ComponentSchema {
+  collectionName: 'components_global_conditions';
+  info: {
+    description: '';
+    displayName: 'conditions';
+    icon: 'oneToMany';
+  };
+  attributes: {
+    score_gte: Schema.Attribute.Integer;
+    score_lte: Schema.Attribute.Integer;
+    source: Schema.Attribute.Enumeration<['all', 'land', 'air']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'all'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'global.conditions': GlobalConditions;
+    }
+  }
+}
