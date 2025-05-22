336c9155d551bacb979c516ea955270047e0b19f
Clément <clement@numericite.eu>
2025-05-22 15:07:11 +0200

    feat(cms): add aws bucket config plugin

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,32 @@
-export default () => ({});
+module.exports = ({ env }) => {
+  const hasAwsCreds =
+    env("AWS_ACCESS_KEY_ID") &&
+    env("AWS_SECRET_ACCESS_KEY") &&
+    env("AWS_BUCKET") &&
+    env("AWS_REGION");
+
+  const isUsingAws = hasAwsCreds && env("NODE_ENV") === "production";
+
+  return {
+    upload: {
+      config: isUsingAws
+        ? {
+            provider: "aws-s3",
+            providerOptions: {
+              accessKeyId: env("AWS_ACCESS_KEY_ID"),
+              secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+              region: env("AWS_REGION"),
+              params: {
+                Bucket: env("AWS_BUCKET"),
+              },
+            },
+          }
+        : {
+            provider: "local",
+            providerOptions: {
+              sizeLimit: 1000000,
+            },
+          },
+    },
+  };
+};
