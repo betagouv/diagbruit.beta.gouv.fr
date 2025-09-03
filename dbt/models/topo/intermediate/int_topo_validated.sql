77404357aa21316e67dfa841689893f4be3a8c41
Clément <clement@numericite.eu>
2025-09-03 14:24:22 +0200

    feat(dbt): add specific dbt pipeline for topo

---
+++ dbt/models/topo/intermediate/int_topo_validated.sql
@@ -0,0 +1,73 @@
+{{ config(
+    materialized='view',
+    schema='workspace'
+) }}
+
+WITH exploded_geometries AS (
+    SELECT
+        id,
+        nature,
+        usage1,
+        usage2,
+        leger,
+        etat,
+        date_creat,
+        date_maj,
+        date_app,
+        date_conf,
+        source,
+        id_source,
+        acqu_plani,
+        acqu_alti,
+        prec_plani,
+        prec_alti,
+        nb_logts,
+        nb_etages,
+        mat_murs,
+        mat_toits,
+        hauteur,
+        z_min_sol,
+        z_min_toit,
+        z_max_toit,
+        z_max_sol,
+        origin_bat,
+        app_ff,
+        ids_rnb,
+        (ST_Dump(geometry)).geom AS geometry,
+        (ST_Dump(geometry)).path[1] AS geom_idx
+    FROM {{ ref('stg_topo') }}
+)
+
+SELECT
+    id,
+    nature,
+    usage1,
+    usage2,
+    leger,
+    etat,
+    date_creat,
+    date_maj,
+    date_app,
+    date_conf,
+    source,
+    id_source,
+    acqu_plani,
+    acqu_alti,
+    prec_plani,
+    prec_alti,
+    nb_logts,
+    nb_etages,
+    mat_murs,
+    mat_toits,
+    hauteur,
+    z_min_sol,
+    z_min_toit,
+    z_max_toit,
+    z_max_sol,
+    origin_bat,
+    app_ff,
+    ids_rnb,
+    geom_idx,
+    geometry,
+    {{ validate_geometry('geometry') }}
+FROM exploded_geometries
