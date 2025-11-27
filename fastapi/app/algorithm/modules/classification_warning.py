e0b8ba25eff9bf7ebdd9953d094f108672d98b4b
Clément <clement@numericite.eu>
2025-11-27 11:07:45 +0100

    fix(fastapi): classification warning taking industy into account

--- fastapi/app/algorithm/modules/classification_warning.py
+++ fastapi/app/algorithm/modules/classification_warning.py
@@ -5,7 +5,7 @@ def get_classification_warning(noisemap_intersections, soundclassification_inter
     noisemap_typesources = {
         item["typesource"]
         for item in noisemap_intersections
-        if item.get("cbstype") == "A" and "typesource" in item
+        if item.get("cbstype") == "A" and "typesource" in item and item["typesource"] in ['F', 'R']
     }
     classification_typesources = {item["typesource"] for item in soundclassification_intersections if "typesource" in item}
     return not noisemap_typesources.issubset(classification_typesources)
