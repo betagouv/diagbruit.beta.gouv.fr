38ea27f382d42f9e34eb95a28245f55e425c684b
Clément <clement@numericite.eu>
2025-12-09 14:03:06 +0100

    tmp: test noisesource intersection

---
+++ fastapi/app/models/noisesource.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text
+from geoalchemy2 import Geometry
+from ..database import Base
+
+
+class NoiseSourceItem(Base):
+    """Model representing noise source data items"""
+    __tablename__ = "noisesource"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    label = Column(Text)
+    geometry = Column(Geometry('GEOMETRY', srid=4326))
+    category_slug = Column(Text, index=True)
