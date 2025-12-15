b760a7ff4f7b40aab2e9b1bc6dd5741f981973c7
Clément <clement@numericite.eu>
2025-12-15 17:16:43 +0100

    feat(cms): add description for noisesource categories in cms

--- cms/types/generated/contentTypes.d.ts
+++ cms/types/generated/contentTypes.d.ts
@@ -491,6 +491,7 @@ export interface ApiNoiseSourceCategoryNoiseSourceCategory
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
