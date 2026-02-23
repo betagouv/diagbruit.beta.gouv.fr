bb01032b1ac36d2191662b237fbcb14641e751f5
Clément Lelong <clement@numericite.eu>
2026-02-23 11:51:13 +0100

    feat(frontend): add tally iframe

--- frontend/src/components/diagnostic/Diagnostic.tsx
+++ frontend/src/components/diagnostic/Diagnostic.tsx
@@ -111,6 +111,18 @@ const Diagnostic = ({ diagnosticItem }: DiagnosticProps) => {
             hint="Terrasses / bars , écoles, industries, ralentisseurs, marchés, carrossiers et équipements sportifs"
           />
           <DiagnosticLocalNoiseSources diagnosticItem={diagnosticItem} />
+          <div className={fr.cx("fr-card", "fr-mt-8v", "fr-p-4v")}>
+            <iframe
+              data-tally-src="https://tally.so/embed/1A4kZL?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+              loading="lazy"
+              width="100%"
+              height={275}
+              frameBorder="0"
+              marginHeight={0}
+              marginWidth={0}
+              title="Votre avis sur diagBruit"
+            />
+          </div>
         </>
       ),
     },
@@ -180,6 +192,34 @@ const Diagnostic = ({ diagnosticItem }: DiagnosticProps) => {
       `${diagnosticItem.parcelle.code_insee}-${diagnosticItem.parcelle.section}-${diagnosticItem.parcelle.numero}`,
     );
   }, [diagnosticItem]);
+
+  useEffect(() => {
+    const w = "https://tally.so/widgets/embed.js";
+    const loadEmbeds = () => {
+      if (typeof (window as any).Tally !== "undefined") {
+        (window as any).Tally.loadEmbeds();
+      } else {
+        document
+          .querySelectorAll("iframe[data-tally-src]:not([src])")
+          .forEach((e) => {
+            (e as HTMLIFrameElement).src =
+              (e as HTMLIFrameElement).dataset.tallySrc || "";
+          });
+      }
+    };
+
+    if (typeof (window as any).Tally !== "undefined") {
+      loadEmbeds();
+    } else if (!document.querySelector(`script[src="${w}"]`)) {
+      const s = document.createElement("script");
+      s.src = w;
+      s.onload = loadEmbeds;
+      s.onerror = loadEmbeds;
+      document.body.appendChild(s);
+    } else {
+      loadEmbeds();
+    }
+  }, []);
 
   return (
     <div id="diagnostic-section">
