9c39a1925e6a948b830a4403e64a5bf503c39185
Clément <clement@numericite.eu>
2026-03-04 18:38:55 +0100

    fix: add posthook with specific index for noisesource

--- dbt/models/osm/marts/noisesource.sql
+++ dbt/models/osm/marts/noisesource.sql
@@ -1,6 +1,9 @@
 {{ config(
     materialized='table',
-    post_hook="ALTER TABLE {{ this }} ADD COLUMN id serial4 PRIMARY KEY;"
+    post_hook=[
+        "ALTER TABLE {{ this }} ADD COLUMN id serial4 PRIMARY KEY;",
+        "CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_geometry ON {{ this }} USING GIST (geometry);"
+    ]
 ) }}
 
 SELECT
