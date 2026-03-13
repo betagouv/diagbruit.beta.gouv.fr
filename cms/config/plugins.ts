175f98b60643e9897a3547ac66dd691a57e53025
Clément <clement@numericite.eu>
2026-03-13 11:32:05 +0100

    fix: email plugin user/pass

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -11,34 +11,42 @@ module.exports = ({ env }) => {
     upload: {
       config: isUsingAws
         ? {
-          provider: "aws-s3",
-          providerOptions: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-            region: env("AWS_REGION"),
-            params: {
-              Bucket: env("AWS_BUCKET"),
+            provider: "aws-s3",
+            providerOptions: {
+              accessKeyId: env("AWS_ACCESS_KEY_ID"),
+              secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+              region: env("AWS_REGION"),
+              params: {
+                Bucket: env("AWS_BUCKET"),
+              },
+            },
+          }
+        : {
+            provider: "local",
+            providerOptions: {
+              sizeLimit: 1000000,
             },
           },
-        }
-        : {
-          provider: "local",
-          providerOptions: {
-            sizeLimit: 1000000,
-          },
-        },
     },
     email: {
       config: {
-        provider: 'nodemailer',
+        provider: "nodemailer",
         providerOptions: {
-          host: env('NODEMAILER_HOST'),
-          port: env.int('NODEMAILER_PORT', 587),
+          host: env("NODEMAILER_HOST"),
+          port: env.int("NODEMAILER_PORT", 587),
           secure: false,
+          ...(env("NODEMAILER_USER") && env("NODEMAILER_PASS")
+            ? {
+                auth: {
+                  user: env("NODEMAILER_USER"),
+                  pass: env("NODEMAILER_PASS"),
+                },
+              }
+            : {}),
         },
         settings: {
-          defaultFrom: env('NODEMAILER_FROM'),
-          defaultReplyTo: 'support@example.com',
+          defaultFrom: env("NODEMAILER_FROM"),
+          defaultReplyTo: "support@example.com",
         },
       },
     },
