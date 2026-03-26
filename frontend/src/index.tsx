0595824e3748d5276357233cd0803476889a5310
SenhaoNum <ben659@hotmail.com>
2026-03-26 11:08:09 +0100

    Feat : added mediatheque slug route
    Feat : added mediatheque slug route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,6 +11,7 @@ import CmsPage from "./pages/cms-page";
 import Diagnostic from "./pages/diagnostic";
 import Home from "./pages/home";
 import Maintenance from "./pages/maintenance";
+import MediathequePage from "./pages/mediatheque";
 import Stats from "./pages/stats";
 import reportWebVitals from "./reportWebVitals";
 
@@ -30,6 +31,7 @@ const App = () => {
         <Route path="/changelogs" element={<Changelogs />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="/maintenance" element={<Maintenance />} />
+        <Route path="/mediatheque/:slug" element={<MediathequePage />} />
         <Route
           path="/accessibility"
           element={<CmsPage slug="accessibility" />}
