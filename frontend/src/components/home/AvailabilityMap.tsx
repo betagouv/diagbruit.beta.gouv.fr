7b6728160efde877a1d22f25c30c59aa4314743a
SenhaoNum <ben659@hotmail.com>
2026-03-31 14:16:02 +0200

    Feat : AvailabilityMap now uses global content
    Feat : AvailabilityMap now uses global content

--- frontend/src/components/home/AvailabilityMap.tsx
+++ frontend/src/components/home/AvailabilityMap.tsx
@@ -1,27 +1,27 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { tss } from "tss-react/dsfr";
 
-export const AvailabilityMap = () => {
+export interface AvailabilityMapProps {
+    title: string;
+    textContent: string;
+}
+
+export const AvailabilityMap = ({ content }: { content: AvailabilityMapProps }) => {
     const { cx, classes } = useStyles();
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(content.textContent, "text/html");
+    const text = doc.body.innerHTML;
 
     return (
         <div className={cx(classes.contentContainer)}>
             <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
                 <div className="fr-col-6">
-                    <h1>Carte de disponibilité du diagnostic de bruit</h1>
-                    <p>diagbruit croise données nationales pour évaluer l'environnement sonore.
-                        Ces données sont disponible dans 5 départements, dont les villes de <strong>Bordeaux, Strasbourg, Rennes, Lille et Nantes.</strong>
-                    </p>
-                    <p>
-                        <strong>
-                            Ouvertures prochaines : Marseille et Nîmes
-                        </strong>
-                    </p>
-                    <p>
-                        Vous aimeriez diagbruit dans votre ville ? <a href="#" className={fr.cx("fr-link")} >
-                            Dites-le-nous
-                        </a>
-                    </p>
+                    <h1>{content.title}</h1>
+                    <div
+                        className={cx(classes.textContent)}
+                        dangerouslySetInnerHTML={{ __html: text }}
+                    />
                     <a href="#" className={fr.cx("fr-link", "fr-icon-arrow-right-line", "fr-link--icon-right",)}>
                         Consulter nos statistiques
                     </a>
@@ -44,6 +44,11 @@ const useStyles = tss.withName(AvailabilityMap.name).create(() => ({
         paddingLeft: "calc(50vw - 50%)",
         paddingRight: "calc(50vw - 50%)",
         backgroundColor: fr.colors.decisions.background.alt.blueFrance.default,
+    },
+    textContent: {
+        a: {
+            color: fr.colors.decisions.background.flat.blueFrance.default,
+        }
     }
 }));
 
