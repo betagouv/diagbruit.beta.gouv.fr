2e54a38c2d486316c3614b0207451d136a82ff48
SenhaoNum <ben659@hotmail.com>
2026-04-16 17:04:05 +0200

    Feat : safer diagPrivew
    Feat : safer diagPrivew

--- frontend/src/components/home/About.tsx
+++ frontend/src/components/home/About.tsx
@@ -29,7 +29,7 @@ export const About = ({ content, partners }: { content: AboutHomePageProps, part
     const parser = new DOMParser();
     const doc = parser.parseFromString(content.textContent, "text/html");
     const text = doc.body.innerHTML;
-    const url = content.profilePicture.url ? imgUrl(content.profilePicture?.url) : "/images/authorDefaultImage.png";
+    const url = content.profilePicture?.url ? imgUrl(content.profilePicture?.url) : "/images/authorDefaultImage.png";
 
     return (
         <div className={cx(classes.contentContainer)}>
