5d7286a96de9d1569703bba304d75aea4cc5edfb
SenhaoNum <ben659@hotmail.com>
2026-04-28 14:27:53 +0200

    Fix typo
    Fix typo

--- dagster_project/src/dagster_project/definitions.py
+++ dagster_project/src/dagster_project/definitions.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from dagster import Definitions, EnvVar, definitions, load_from_defs_folder
-from dagster_project.defs.ressources.box import BoxResource
+from dagster_project.defs.resources.box import BoxResource
 
 
 @definitions
