5e1c8b318f85f8b0e8bc3301130f614de19c060c
SenhaoNum <ben659@hotmail.com>
2026-04-28 16:27:53 +0200

    Feat : added token storage in db and now using OAuth
    Feat : added token storage in db and now using OAuth

--- dagster_project/src/dagster_project/definitions.py
+++ dagster_project/src/dagster_project/definitions.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dagster import Definitions, EnvVar, definitions, load_from_defs_folder
 from dagster_project.defs.resources.box import BoxResource
 
+from dagster_project.defs.jobs.tools import _db_url
 
 @definitions
 def defs():
@@ -13,10 +14,7 @@ def defs():
                 "box": BoxResource(
                     client_id=EnvVar("BOX_CLIENT_ID"),
                     client_secret=EnvVar("BOX_CLIENT_SECRET"),
-                    jwt_key_id=EnvVar("BOX_JWT_KEY_ID"),
-                    private_key=EnvVar("BOX_PRIVATE_KEY"),
-                    private_key_passphrase=EnvVar("BOX_PRIVATE_KEY_PASSPHRASE"),
-                    user_id=EnvVar("BOX_USER_ID"),
+                    db_url=_db_url(),
                 )
             }
         ),
