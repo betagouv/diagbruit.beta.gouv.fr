ae8b039314c542ab03abc84f59a1187dab983871
SenhaoNum <ben659@hotmail.com>
2026-04-29 10:31:18 +0200

    Feat : added jobs for agglo and infra
    Feat : added jobs for agglo and infra

--- dagster_project/src/dagster_project/defs/jobs/defs.py
+++ dagster_project/src/dagster_project/defs/jobs/defs.py
@@ -10,6 +10,22 @@ full_landing_job = define_asset_job(
     selection=AssetSelection.groups("landing"),
 )
 
+dept_033 = define_asset_job(
+    name="dept_033",
+    selection= AssetSelection.keys("agglo_033_launcher") 
+    | AssetSelection.keys("agglo_033_landing") 
+    | AssetSelection.keys("infra_033_launcher") 
+    | AssetSelection.keys("infra_033_landing")
+)
+
+dept_044 = define_asset_job(
+    name="dept_044",
+    selection= AssetSelection.keys("agglo_044_launcher") 
+    | AssetSelection.keys("agglo_044_landing") 
+    | AssetSelection.keys("infra_044_launcher") 
+    | AssetSelection.keys("infra_044_landing")
+)
+
 full_ingestion_job = define_asset_job(
     name="full_ingestion_job",
     selection=AssetSelection.keys("raw_full_stras_data") 
