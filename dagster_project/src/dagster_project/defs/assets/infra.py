43236c2a7ef390dd0c69e3a7e83c0092b0f21793
SenhaoNum <ben659@hotmail.com>
2026-05-04 14:48:28 +0200

    Refact : changed data url arr
    Refact : changed data url arr

--- dagster_project/src/dagster_project/defs/assets/infra.py
+++ dagster_project/src/dagster_project/defs/assets/infra.py
@@ -2,18 +2,7 @@ from dagster import AssetExecutionContext, asset
 from dagster_project.defs.jobs.tools import s3_launcher, s3_landing
 
 DEPT033_URL = [
-    "https://www.data.gouv.fr/api/1/datasets/r/17e3754b-b23a-4c5d-be5c-becb000a9d4c",
-    "https://www.data.gouv.fr/api/1/datasets/r/7bc3ddc2-cded-4b4e-bdc5-0c270ecb6201",
-    "https://www.data.gouv.fr/api/1/datasets/r/00872b29-5e87-4d24-ba33-28c0faa808a1",
-    "https://www.data.gouv.fr/api/1/datasets/r/b5033591-4cbc-4160-87d7-fae72776ac62",
-    "https://www.data.gouv.fr/api/1/datasets/r/58ea856f-99e7-4a49-b20d-432076a430c7",
-    "https://www.data.gouv.fr/api/1/datasets/r/3bcd60ce-0a1d-4500-bf73-73e20d65af9b",
-    "https://www.data.gouv.fr/api/1/datasets/r/c680c193-25ac-46ed-907c-05962f260088",
-    "https://www.data.gouv.fr/api/1/datasets/r/18e1bb2e-c5ea-4e09-abd7-f07adb3ced94",
-    "https://www.data.gouv.fr/api/1/datasets/r/0598a10c-89f0-4eae-8b8f-7ecda5e770be",
-    "https://www.data.gouv.fr/api/1/datasets/r/ad82bc06-5f23-4ca0-935f-8f16feba630b",
-    "https://www.data.gouv.fr/api/1/datasets/r/d7546128-fb23-4cbd-a43a-42d0e2890286",
-    "https://www.data.gouv.fr/api/1/datasets/r/5cb02473-5c7a-416a-a883-c22fce652819"
+    "https://www.data.gouv.fr/api/1/datasets/r/b4cf0f5e-4b99-4af3-916e-1d8c2625fce2"
 ]
 
 DEPT044_URL = [
