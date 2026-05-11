c125eeb9f75710047aa3615177a0b961f862d981
Clément Lelong <clement@numericite.eu>
2026-05-11 12:01:53 +0200

    fix: undefined risk wording for summary

--- frontend/src/utils/tools.ts
+++ frontend/src/utils/tools.ts
@@ -103,6 +103,10 @@ export const getReadableSource = (
 export const getSummaryTextFromDiagnostic = (
   diagnostic: Diagnostic,
 ): string => {
+  if (diagnostic.flags.hasNoisemapWarning) {
+    return "Le <strong>risque de nuisance sonore</strong> ne peut pas être déterminé par diagbruit pour votre parcelle.<br/>Une <strong>visite des lieux</strong> peut aider à mieux appréhender la gêne occasionnée.";
+  }
+
   const risk = getRiskFromScore(diagnostic.score);
 
   switch (risk) {
@@ -477,10 +481,10 @@ export const getMaxIsolationFromSoundClassificationAffectedHelper = (
 ) => {
   return !!optimalZoneSoundClassificationHelper.length
     ? Math.max(
-      ...optimalZoneSoundClassificationHelper.map(
-        (helper) => helper.isolation,
-      ),
-    )
+        ...optimalZoneSoundClassificationHelper.map(
+          (helper) => helper.isolation,
+        ),
+      )
     : 0;
 };
 
@@ -488,15 +492,18 @@ export const getIsMobile = () => {
   const { breakpointsValues } = useBreakpointsValuesPx();
   const { windowInnerWidth } = useWindowInnerSize();
   return windowInnerWidth < breakpointsValues.md;
-}
+};
 
 export const normalize = (str: string) =>
-  str.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase();
+  str
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")
+    .toLowerCase();
 
 export const slugify = (str: string) =>
-  normalize(str).replace(/[^a-z0-9]+/g, "-").replace(/^-|-$/g, "");
+  normalize(str)
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "");
 
 export const imgUrl = (url: string) =>
-  url.startsWith("/")
-    ? `${process.env.REACT_APP_CMS_URL}${url}`
-    : url
+  url.startsWith("/") ? `${process.env.REACT_APP_CMS_URL}${url}` : url;
