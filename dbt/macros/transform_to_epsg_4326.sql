e3c911269611d39f9e57fd78d29f30bc85156f7a
SenhaoNum <ben659@hotmail.com>
2026-06-03 11:56:08 +0200

    Refact : deleted root dbt folder
    Refact : deleted root dbt folder

--- dbt/macros/transform_to_epsg_4326.sql
+++
@@ -1,3 +0,0 @@
-{% macro transform_to_epsg_4326(geometry_column, source_srid=2154) %}
-    ST_Transform({{ geometry_column }}, 4326)
-{% endmacro %}
--- dbt/models/noisemap/intermediate/int_noisemap_validated.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='view',
-    schema='workspace'
-) }}
-
-SELECT
-    *,
-    {{ validate_geometry('geometry') }}
-FROM {{ ref('int_noisemap_exploded') }}
