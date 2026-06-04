6fda8dc3fa0de4f0f87f35c8637002311ba67937
SenhaoNum <ben659@hotmail.com>
2026-06-04 11:54:12 +0200

    Refact : bdnd dbt copy of topo
    Refact : bdnd dbt copy of topo

---
+++ dagster_project/dbt/models/bdnb/intermediate/int_bdnb_projected.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='view',
+    schema='workspace'
+) }}
+
+SELECT
+    fid,
+    batiment_c,
+    batiment_g,
+    code_depar,
+    code_iris,
+    code_commu,
+    s_geom_cst,
+    hauteur,
+    altitude_s,
+    geom_idx,
+    {{ transform_to_epsg_4326('geometry') }} AS geometry,
+    area_m2,
+    4326 AS srid,
+    original_is_valid,
+    original_validity_reason,
+    is_valid_now,
+    geometry_type
+FROM {{ ref('int_bdnb_fixed') }}
