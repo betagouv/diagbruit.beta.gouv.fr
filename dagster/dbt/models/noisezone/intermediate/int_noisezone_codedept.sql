374deaee295dd3fee1ed10c7c2676d5556520f1e
SenhaoNum <ben659@hotmail.com>
2026-06-16 17:00:57 +0200

    Feat : added fixed sql in dbt pipeline
    Feat : added fixed sql in dbt pipeline

--- dagster/dbt/models/noisezone/intermediate/int_noisezone_codedept.sql
+++ dagster/dbt/models/noisezone/intermediate/int_noisezone_codedept.sql
@@ -8,7 +8,7 @@ WITH geo_enriched AS (
     SELECT
         n.*,
         LPAD(d.code, 3, '0') AS codedept
-    FROM {{ ref('int_noisezone_validated') }} n
+    FROM {{ ref('int_noisezone_fixed') }} n
     JOIN {{ source('public_workspace', 'geo_departements') }} d
         ON n.geometry && d.geometry
         AND ST_Intersects(d.geometry, n.geometry)
