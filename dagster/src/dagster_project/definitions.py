287d3b38def9e375a932d12ac1390d9241214e84
SenhaoNum <ben659@hotmail.com>
2026-06-17 11:20:18 +0200

    Refact : replaced user_id with enterprise_id
    Refact : replaced user_id with enterprise_id and got rid of deprecated box_auth.py script

--- dagster/src/dagster_project/definitions.py
+++ dagster/src/dagster_project/definitions.py
@@ -14,7 +14,7 @@ def defs():
                 "box": BoxResource(
                     client_id=EnvVar("BOX_CLIENT_ID"),
                     client_secret=EnvVar("BOX_CLIENT_SECRET"),
-                    user_id=EnvVar("BOX_USER_ID"),
+                    enterprise_id=EnvVar("BOX_ENTREPRISE_ID"),
                 )
             }
         ),
--- dagster/src/dagster_project/defs/schedules/box_refresh.py
+++ dagster/src/dagster_project/defs/schedules/box_refresh.py
@@ -1,6 +1,5 @@
 from dagster import (
     AssetExecutionContext,
-    Config,
     MaterializeResult,
     MetadataValue,
     asset,
@@ -26,25 +25,3 @@ box_connectivity_check_job = define_asset_job(
     "box_connectivity_check_job",
     selection=["box_connectivity_check"],
 )
-
-
-class BoxFolderInspectConfig(Config):
-    folder_id: str = "0"  # Box root folder; override with any folder ID
-
-
-@asset(
-    key="box_folder_inspect",
-    group_name="maintenance",
-    kinds={"box"},
-)
-def box_folder_inspect(context: AssetExecutionContext, config: BoxFolderInspectConfig, box: BoxResource):
-    client = box.get_client()
-    items = client.folders.get_folder_items(config.folder_id)
-    names = [item.name for item in items.entries]
-    for name in names:
-        context.log.info(name)
-    return MaterializeResult(metadata={
-        "folder_id": MetadataValue.text(config.folder_id),
-        "file_count": MetadataValue.int(len(names)),
-        "files": MetadataValue.json(names),
-    })
