1684cb21745e907b040625c1e87d62845aa7b109
SenhaoNum <ben659@hotmail.com>
2026-06-24 11:43:00 +0200

    Fix : diag link error
    Fix : diag link error

--- frontend/src/pages/diagnostic.tsx
+++ frontend/src/pages/diagnostic.tsx
@@ -225,6 +225,7 @@ function DiagnosticPage() {
           typeof addressFeature === "object" &&
           "geometry" in addressFeature
         ) {
+          reset();
           if (mapMethodsRef.current?.map) {
             setAddressDefaultValue(addressFeature);
             mapMethodsRef.current.map.flyTo({
