6527ce0b378e06d3d37a66c93b871db0317ed8f8
SenhaoNum <ben659@hotmail.com>
2026-07-08 10:37:18 +0200

    Feat : added new strapi component
    Feat : added new strapi component email profiles

---
+++ cms/src/api/email-form-profile/controllers/email-form-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * email-form-profile controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::email-form-profile.email-form-profile');
--- cms/types/generated/contentTypes.d.ts
+++ cms/types/generated/contentTypes.d.ts
@@ -517,6 +517,36 @@ export interface ApiDecreeDecree extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiEmailFormProfileEmailFormProfile
+  extends Struct.SingleTypeSchema {
+  collectionName: 'email_form_profiles';
+  info: {
+    description: '';
+    displayName: 'EmailFormProfile';
+    pluralName: 'email-form-profiles';
+    singularName: 'email-form-profile';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    EmailProfiles: Schema.Attribute.Component<'global.email-profiles', true>;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::email-form-profile.email-form-profile'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiEmailEmail extends Struct.CollectionTypeSchema {
   collectionName: 'emails';
   info: {
@@ -1382,6 +1412,7 @@ declare module '@strapi/strapi' {
       'api::acoustic-certificate.acoustic-certificate': ApiAcousticCertificateAcousticCertificate;
       'api::changelog.changelog': ApiChangelogChangelog;
       'api::decree.decree': ApiDecreeDecree;
+      'api::email-form-profile.email-form-profile': ApiEmailFormProfileEmailFormProfile;
       'api::email.email': ApiEmailEmail;
       'api::home-page-content.home-page-content': ApiHomePageContentHomePageContent;
       'api::legal-mention.legal-mention': ApiLegalMentionLegalMention;
